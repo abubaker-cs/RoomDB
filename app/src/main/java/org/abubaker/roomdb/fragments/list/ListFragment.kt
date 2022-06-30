12a5f72f44e90e3800ec8d821ae4fa510844316e
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-06-30 19:44:22 +0500

    Working on the ListFragment.kt

--- app/src/main/java/org/abubaker/roomdb/fragments/list/ListFragment.kt
+++ app/src/main/java/org/abubaker/roomdb/fragments/list/ListFragment.kt
@@ -10,12 +10,20 @@ import org.abubaker.roomdb.R
 
 class ListFragment : Fragment() {
 
+    // private var _binding: ListFragmentBinding? = null
+    // private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        val view = inflater.inflate(R.layout.fragment_list, container, false)
+
+
+
+        return view
     }
 
 }
