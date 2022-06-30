d765b282e5935eb926050411d840f26b53c5fb0e
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-06-30 23:59:58 +0500

    The incompatibility between Kotlin and Room versions was cause suspend to not work in @Dao

--- app/src/main/java/org/abubaker/roomdb/data/UserDao.kt
+++ app/src/main/java/org/abubaker/roomdb/data/UserDao.kt
@@ -12,7 +12,7 @@ interface UserDao {
 
     // Insert a User into the database by using the OnConflictStrategy.IGNORE
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun addUser(vararg user: User)
+    suspend fun addUser(user: User)
 
     // Retrieve all Users from the database
     @Query("SELECT * FROM user_table ORDER BY id ASC")
