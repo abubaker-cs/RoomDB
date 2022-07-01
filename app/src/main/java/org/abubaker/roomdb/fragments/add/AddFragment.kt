40d4cddd9cdfce6f71735417faf43898bd2001d5
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-07-02 02:14:47 +0500

    Working...

--- app/src/main/java/org/abubaker/roomdb/fragments/add/AddFragment.kt
+++ app/src/main/java/org/abubaker/roomdb/fragments/add/AddFragment.kt
@@ -6,9 +6,12 @@ import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import org.abubaker.roomdb.databinding.FragmentAddBinding
+import org.abubaker.roomdb.model.User
 import org.abubaker.roomdb.viewmodel.UserViewModel
 
 
@@ -37,11 +40,6 @@ class AddFragment : Fragment() {
 
             insertDataToDatabase()
 
-            // Fragment: Add Item
-            // findNavController().navigate(
-            //    AddFragmentDirections.actionAddItemFragmentToItemListFragment()
-            // )
-
         }
 
     }
@@ -50,10 +48,32 @@ class AddFragment : Fragment() {
         val firstName = binding.etFirstName.text.toString()
         val lastName = binding.etLastName.text.toString()
         val age = binding.etAge.text
+
+        if (inputCheck(firstName, lastName, age)) {
+
+            // Create user object
+            val user = User(0, firstName, lastName, Integer.parseInt(age.toString()))
+
+            // Add user to the Database
+            mUserViewModel.addUser(user)
+
+            // Confirmation
+            Toast.makeText(requireContext(), "Successfully new record added", Toast.LENGTH_SHORT)
+                .show()
+
+            // Navigate back to the List view
+            findNavController().navigate(
+                AddFragmentDirections.actionAddItemFragmentToItemListFragment()
+            )
+
+        }
+
     }
 
     private fun inputCheck(firstName: String, lastName: String, age: Editable): Boolean {
-        return !(TextUtils.isEmpty(firstName) && TextUtils.isEmpty(lastName))
+        return !(TextUtils.isEmpty(firstName) && TextUtils.isEmpty(lastName) && TextUtils.isEmpty(
+            age
+        ))
     }
 
 }
