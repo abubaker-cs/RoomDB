66bdb9c986c01f1c753a4eb719987bde3f7647c1
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-07-02 02:09:37 +0500

    Working...

--- app/src/main/java/org/abubaker/roomdb/fragments/add/AddFragment.kt
+++ app/src/main/java/org/abubaker/roomdb/fragments/add/AddFragment.kt
@@ -1,17 +1,23 @@
 package org.abubaker.roomdb.fragments.add
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import org.abubaker.roomdb.databinding.FragmentAddBinding
+import org.abubaker.roomdb.viewmodel.UserViewModel
 
 
 class AddFragment : Fragment() {
 
     private var _binding: FragmentAddBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var mUserViewModel: UserViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,6 +26,34 @@ class AddFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentAddBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mUserViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
+
+        binding.btnAdd.setOnClickListener {
+
+            insertDataToDatabase()
+
+            // Fragment: Add Item
+            // findNavController().navigate(
+            //    AddFragmentDirections.actionAddItemFragmentToItemListFragment()
+            // )
+
+        }
+
+    }
+
+    private fun insertDataToDatabase() {
+        val firstName = binding.etFirstName.text.toString()
+        val lastName = binding.etLastName.text.toString()
+        val age = binding.etAge.text
+    }
+
+    private fun inputCheck(firstName: String, lastName: String, age: Editable): Boolean {
+        return !(TextUtils.isEmpty(firstName) && TextUtils.isEmpty(lastName))
     }
 
 }
